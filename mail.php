e53acf2188047d1ae0e007ebf2a6c0c4ec512e60
No-_Name <87339058+N0Namr3@users.noreply.github.com>
2021-11-13 23:47:31 +0100

    Add files via upload

--- mail.php
+++ mail.php
@@ -6,12 +6,12 @@ $email= $_POST['emailaddress'];
 $message= $_POST['message'];
 
 
-$to = "ggpro7351@gmail.com";
+$to = "wayir34572@incoware.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
 
 
-$headers = "From: noreply@https://n0namr3.github.io/No-Name/" . "\r\n" .
+$headers = "From: noreply@yoursite.com" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
